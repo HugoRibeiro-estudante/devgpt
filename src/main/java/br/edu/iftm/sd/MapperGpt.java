200345653ed8d1bdf8ee5645873cc485a4a94a58
HugoRibeiro-estudante <hugo.alves@estudante.iftm.edu.br>
2023-12-15 19:21:54 -0300

    alteracao no mapper

--- src/main/java/br/edu/iftm/sd/MapperGpt.java
+++ src/main/java/br/edu/iftm/sd/MapperGpt.java
@@ -3,16 +3,13 @@ package br.edu.iftm.sd;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-import javax.naming.Context;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-
-public class MapperGpt extends Mapper<Object, Text, Text, IntWritable>{
+public class MapperGpt extends Mapper<Object, Text, Text, IntWritable> {
     
-private final static IntWritable numeroUm = new IntWritable(1);
+    private final static IntWritable numeroUm = new IntWritable(1);
     private final Text palavra = new Text();
 
     @Override
@@ -21,25 +18,24 @@ private final static IntWritable numeroUm = new IntWritable(1);
 
         while (tk.hasMoreTokens()) {
             String token = tk.nextToken();
-            if (token.equals("\"NumberOfPrompts\":")) {
-                String data = tk.nextToken();
-                System.out.println(data);
-                palavra.set(data);
-                context.write(palavra, numeroUm);
-            }
-            if (token.equals("\"Type\":")) {
-                String tipo = tk.nextToken();
-                System.out.println(tipo);
-                palavra.set(tipo);
-                context.write(palavra, numeroUm);
-            }
 
-            if(token.equals("\"DateOfConversation\":")){
-                String date = tk.nextToken();
+            if (token.equals("\"DateOfConversation\":")) {
+                // Combine the next three tokens to get the complete date string
+                String month = removeSpecialChars(tk.nextToken());
+                String day = removeSpecialChars(tk.nextToken());
+                String year = removeSpecialChars(tk.nextToken());
+                
+                String date = month + " " + day + ", " + year;
                 System.out.println(date);
+
                 palavra.set(date);
                 context.write(palavra, numeroUm);
             }
         }
+    }
+
+    private String removeSpecialChars(String input) {
+        // Remove special characters like commas
+        return input.replaceAll("[^a-zA-Z0-9]", "");
     }
 }
